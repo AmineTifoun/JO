33747033cb8608f4e39381b22d7529d9dd858d3a
AmineTifoun <149804027+AmineTifoun@users.noreply.github.com>
2024-02-11 00:29:06 +0100

    db connexion done

--- JO-Back/server.js
+++ JO-Back/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const server = express();
-
+const { createPool }= require("mysql")
 server.use(express.json());
 
 
 /**************** BASE DE DONNEE CONFIGURATION ***************/
 
-
+const pool = createPool({
+    host:"localhost",
+    user:"root",
+    password:"amine",
+    database:"JO",
+    connectionLimit:5
+})
 
 
 /************************************************************* */
